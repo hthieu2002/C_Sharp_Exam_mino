01c6ca9025841140d36a6da2d958ddfb189761ff
TrungHieuMobile <hoangtrunghieu26112002@gmail.com>
2023-09-14 19:27:44 +0700

    update file

--- Exam5/Exam5/Program.cs
+++ Exam5/Exam5/Program.cs
@@ -1,40 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Nhập vào một danh sách các số thập phân, cách nhau bởi dấu cách
-        Console.WriteLine("Nhập vào một danh sách các số thập phân, cách nhau bởi dấu cách:");
-        string input = Console.ReadLine();
-
-        // Chuyển đổi chuỗi thành một mảng các số thập phân
-        decimal[] numbers = input.Split().Select(decimal.Parse).ToArray();
-
-        // Tạo một danh sách để lưu kết quả
-        List<decimal> result = new List<decimal>();
-
-        // Duyệt qua mảng các số thập phân từ trái sang phải
-        for (int i = 0; i < numbers.Length; i++)
+        Console.WriteLine("Nhập vào một chuỗi số, cách nhau bởi dấu cách:");
+        string input = Console.ReadLine(); // Đọc chuỗi từ bàn phím
+        int[] numbers = input.Split(' ').Select(int.Parse).ToArray(); // Chuyển chuỗi thành mảng số nguyên
+        int product = 1; // Khởi tạo biến tích bằng 1
+        for (int i = 0; i < numbers.Length; i++) // Duyệt qua từng số trong mảng
         {
-            // Lấy số hiện tại
-            decimal current = numbers[i];
-
-            // Nếu số hiện tại bằng số kế tiếp, thì cộng chúng lại
-            while (i < numbers.Length - 1 && current == numbers[i + 1])
+            if (i % 2 == 0) // Nếu vị trí của số đó là vị trí chẵn
             {
-                current += numbers[i + 1];
-                i++;
+                product *= numbers[i]; // Nhân tích với số đó
             }
-
-            // Thêm số hiện tại vào danh sách kết quả
-            result.Add(current);
         }
-
-        // In ra kết quả, cách nhau bởi dấu cách
-        Console.WriteLine("Kết quả là:");
-        Console.WriteLine(string.Join(" ", result));
+        Console.WriteLine("Kết quả là: " + product); // In ra kết quả
     }
 }
